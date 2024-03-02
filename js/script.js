73a8c8ca7025a100693f2037046eb6e31671442e
DewanSingh <dewansingh805@gmail.com>
2024-03-02 02:04:36 -0800

    Update script.js
    changed declaration of map in map markers. changed function name to match one on class API key

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ function picturemessage(){
   alert('Look at this handsome guy!')
 }
 
-function dates(){
+function initMap(){
   map = new google.maps.Map(document.getElementById('map'), {
     center: {lat:34.429893, lng:-119.713450},
     zoom: 10
@@ -10,19 +10,19 @@ function dates(){
   
   new google.maps.Marker({
     position: {lat:34.4237643, lng:-119.705686},
-    map: map,
+    map,
     title: "SB Pho",
   });
   
   new google.maps.Marker({
     position: {lat:34.4218042, lng:-119.7052485},
-    map: map,
+    map,
     title: "LOKUM",
   });
   
   new google.maps.Marker({
     position: {lat:34.4226202, lng:-119.7066041},
-    map: map,
+    map,
     title: "Satellite SB",
   });
   
