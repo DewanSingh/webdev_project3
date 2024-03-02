cb21ab97a58f29121f47cec5cfbad171faadd040
DewanSingh <dewansingh805@gmail.com>
2024-03-02 00:37:33 -0800

    Update script.js
    added map, marker and map call command

--- js/script.js
+++ js/script.js
@@ -2,6 +2,20 @@ function picturemessage(){
   alert('Look at this handsome guy!')
 }
 
+function dates(){
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat:34.429893, lng:-119.713450},
+    zoom: 10
+  });
+  
+  new google.maps.Marker({
+    position: {lat:34.4237643, lng:-119.705686},
+    map: map,
+    title: "SB Pho",
+  });
+}
+
 var dewan = document.getElementById('dewan');
 
 dewan.addEventListener('click', picturemessage);
+window.dates = dates;
