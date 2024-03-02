2756bdae35695e023210a7fade48b31a56367a0d
DewanSingh <dewansingh805@gmail.com>
2024-03-02 02:14:50 -0800

    Update script.js
    corrected map declaration, changed to alert test

--- js/script.js
+++ js/script.js
@@ -3,32 +3,10 @@ function picturemessage(){
 }
 
 function initMap(){
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat:34.429893, lng:-119.713450},
-    zoom: 10
-  });
-  
-  new google.maps.marker.AdvancedMarkerElement({
-    position: {lat:34.4237643, lng:-119.705686},
-    map,
-    title: "SB Pho",
-  });
-  
-  new google.maps.marker.AdvancedMarkerElement({
-    position: {lat:34.4218042, lng:-119.7052485},
-    map,
-    title: "LOKUM",
-  });
-  
-  new google.maps.marker.AdvancedMarkerElement({
-    position: {lat:34.4226202, lng:-119.7066041},
-    map,
-    title: "Satellite SB",
-  });
-  
+  alert('it works');
 }
 
 var dewan = document.getElementById('dewan');
 
 dewan.addEventListener('click', picturemessage);
-window.initMap = initMap;
+google.maps.event.addDomListener(window, 'load', initMap);
