572bb5c1fbe412358c234420af2f9acfc335513b
DewanSingh <dewansingh805@gmail.com>
2024-03-02 02:20:25 -0800

    Update script.js
    undo alert test

--- js/script.js
+++ js/script.js
@@ -3,7 +3,28 @@ function picturemessage(){
 }
 
 function initMap(){
-  alert('it works');
+  map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat:34.429893, lng:-119.713450},
+    zoom: 10
+  });
+
+  new google.maps.marker.AdvancedMarkerElement({
+    position: {lat:34.4237643, lng:-119.705686},
+    map,
+    title: "SB Pho",
+  });
+
+  new google.maps.marker.AdvancedMarkerElement({
+    position: {lat:34.4218042, lng:-119.7052485},
+    map,
+    title: "LOKUM",
+  });
+
+  new google.maps.marker.AdvancedMarkerElement({
+    position: {lat:34.4226202, lng:-119.7066041},
+    map,
+    title: "Satellite SB",
+  });
 }
 
 var dewan = document.getElementById('dewan');
