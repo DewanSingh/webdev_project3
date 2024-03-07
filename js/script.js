28f8dddff07f8a7e9893413d0b8899d9ac2c2947
DewanSingh <dewansingh805@gmail.com>
2024-03-06 20:14:18 -0800

    Update script.js
    readd proper marker references

--- js/script.js
+++ js/script.js
@@ -5,44 +5,44 @@ function picturemessage(){
 function initMap() {
   var map = new google.maps.Map(document.getElementById('map'), {
     center: {lat: 34.429893, lng: -119.713450 },
-    zoom: 10
+    zoom: 15
   });
-  
-  var pho = new google.maps.marker.AdvancedMarkerElement({
+
+  var pho = new google.maps.Marker({
     position: { lat:34.4237643, lng:-119.705686 },
     map: map,
     title: "SB Pho"
   });
-
   var parkingSymbol = {
     path: google.maps.SymbolPath.CIRCLE
     map: map
   };
 
-  var lokum = new google.maps.marker.AdvancedMarkerElement({
+  var lokum = new google.maps.Marker({
     position: { lat:34.4218042, lng:-119.7052485 },
+    map: map,
     title: "LOKUM"
   });
 
-  var satellite = new google.maps.marker.AdvancedMarkerElement({
+  var satellite = new google.maps.Marker({
     position: { lat:34.4226202, lng:-119.7066041 },
     map: map,
     title: "Satellite SB"
   });
 
-  var parking = new google.maps.marker.AdvancedMarkerElement({
+  var parking = new google.maps.Marker({
     position: { lat:34.4214242, lng:-119.706124 },
     map: map,
     icon: parkingSymbol,
     title: "Parking"
   });
-
+    
   var datePathCoor = [
     { lat:34.4226202, lng:-119.7066041 },
     { lat:34.4237643, lng:-119.705686 },
     { lat:34.4218042, lng:-119.7052485 },
   ];
-
+  
   var datePath = new google.maps.Polyline({
     path: datePathCoor,
     map: map,
@@ -51,7 +51,7 @@ function initMap() {
     strokeOpacity: 1.0,
     strokeWeight: 2
   });
-
+  
   datePath.setMap(map);
 }
 
