4113d7151cd8ab1ad3710a46994011ef6f0ab802
DewanSingh <dewansingh805@gmail.com>
2024-03-06 21:06:11 -0800

    Update script.js
    added js for picture slider

--- js/script.js
+++ js/script.js
@@ -65,3 +65,30 @@ lokum.setMap(map);
 satellite.setMap(map);
 parking.setMap(map);
 google.maps.event.addDomListener(window, 'load', initMap);
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("slides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
+}
