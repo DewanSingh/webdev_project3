0c0362017862f2cbef7aa5308b316f76e48b2562
DewanSingh <dewansingh805@gmail.com>
2024-03-07 22:36:54 -0800

    Update script.js
    fix for slideshow break

--- js/script.js
+++ js/script.js
@@ -98,5 +98,7 @@ function showSlides(n) {
   }
   
   slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += "active";
+  dots[slideIndex].className += "active";
+  dots[slideIndex-1].className -= "active";
+  dots[slideIndex+1].className -= "active";
 }
